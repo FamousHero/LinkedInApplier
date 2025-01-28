a4b1eea5596fec9e8cfbd40f4a160e7c8bed1594
Daniel Franco <danielfranco2011@gmail.com>
2025-01-28 14:13:21 -0800

    Add sign_in function

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from dotenv import load_dotenv
+
 from selenium import webdriver
+
 from selenium.webdriver import Chrome
-from pathlib import Path
-import undetected_chromedriver as uc
+from selenium.webdriver.chrome.options import Options
+
+from selenium.webdriver.common.by import By
+
 import time
 import os
+from pathlib import Path
 
 debugging = True
 load_dotenv()
@@ -12,10 +17,22 @@ load_dotenv()
 USER_EMAIL=os.getenv("USER_EMAIL")
 USER_PASSWORD=os.getenv("USER_PASSWORD")
 
+def sign_in(driver):
+    fill_input_by_id(driver, "username", USER_EMAIL)
+    fill_input_by_id(driver, "password", USER_PASSWORD)
+    time.sleep(2)
+    sign_in_btn = driver.find_element(By.CSS_SELECTOR, "[aria-label=\"Sign in\"]")
+    sign_in_btn.click()
+
+def fill_input_by_id(driver, id, input_text):
+    element = driver.find_element(By.ID, id)
+    element.send_keys(input_text)
 
 
-driver = uc.Chrome()
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+chrome_driver = webdriver.Chrome(options=chrome_options)
 
-driver.get('https://www.google.com/')
-
-time.sleep(5)
+chrome_driver.get('https://www.linkedin.com/login')
+sign_in(chrome_driver)
+time.sleep(2)
