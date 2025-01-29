9b92ab488939c0090e046e6eb71acf920b9c0a4a
Daniel Franco <danielfranco2011@gmail.com>
2025-01-28 16:15:52 -0800

    Magic job search url

--- main.py
+++ main.py
@@ -35,4 +35,5 @@ chrome_driver = webdriver.Chrome(options=chrome_options)
 
 chrome_driver.get('https://www.linkedin.com/login')
 sign_in(chrome_driver)
+chrome_driver.get('https://www.linkedin.com/jobs/search/?currentJobId=3959299127&distance=25&f_PP=102277331%2C102448103&f_TPR=r6400&keywords=django&origin=JOB_COLLECTION_PAGE_SEARCH_BUTTON&refresh=true')
 time.sleep(2)
